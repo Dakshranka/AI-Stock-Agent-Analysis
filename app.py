0b56b18715978d2ede3c00f1ccd0c206afe1c526
DakshRanka <rankadaksh4@gmail.com>
2025-02-24 20:59:59 +0530

    update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import requests
 from datetime import datetime, timedelta
 import speech_recognition as sr
 from autogen.agentchat import UserProxyAgent
-from langchain.chat_models import ChatOpenAI
 
 # 🔹 Disable Docker for AutoGen
 user_agent = UserProxyAgent("UserAgent", human_input_mode="ALWAYS", code_execution_config={"use_docker": False})
